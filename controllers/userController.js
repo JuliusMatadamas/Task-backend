2e1867a74190252fe99b36f9dcf2a9267933d491
JuliusMatadamas <julius3877@gmail.com>
2022-06-14 14:22:48 -0500

    Se agrega bcryptjs.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
 
 exports.userCreate = async (req, res) => {
     // extraer el email y el password
@@ -12,7 +13,12 @@ exports.userCreate = async (req, res) => {
             return res.status(400).json({msg: 'Error: There is already a user with the email ' + email});
         }
 
+        // Se crea la instancia de User
         user = new User(req.body);
+
+        // Hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        user.password = await bcryptjs.hash(password, salt);
 
         await user.save();
 
